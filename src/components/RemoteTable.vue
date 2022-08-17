e0e5a50588a800ca43d07250b4e7543f347f2b33
cloud.luo <cloud.luo@starlight-sms.com>
2022-08-17 18:05:12 +0800

    feat:profile page.

--- src/components/RemoteTable.vue
+++ src/components/RemoteTable.vue
@@ -1,36 +1,82 @@
 <template>
     <div>
-        <n-data-table :data="state.data.list" :columns="props.columns" bordered></n-data-table>
+        <n-data-table :loading="state.getDataLoading" :data="state.data.list" :columns="props.columns" bordered
+            :pagination="pagination">
+        </n-data-table>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, reactive, defineProps } from 'vue';
+import { onMounted, reactive, defineProps, computed, nextTick } from 'vue';
 import { DataTableColumns } from 'naive-ui'
+import { Api, PaginationData, request } from '@/utils';
+
+const pagination = computed(() => ({
+    "item-count": state.data.totalCount,
+    page: state.page,
+    "page-size": state.pageSize,
+    "on-update:page": handlePageChange,
+    "on-update:page-size": handlePageSizeChange,
+}))
 
 const props = defineProps<{
-    columns: DataTableColumns
+    columns: DataTableColumns,
+    api: Api,
+    params?: any,
+    pageSize?: number
 }>()
 
-const state = reactive({
+const state = reactive<{
+    getDataLoading: boolean,
+    data: PaginationData,
+    page: number,
+    pageSize: number
+}>({
     getDataLoading: false,
     data: {
         list: [],
         totalCount: 0
-    }
+    },
+    page: 1,
+    pageSize: props.pageSize || 20
 })
 
 const getData = async () => {
     state.getDataLoading = true;
     try {
+        const data = await request<PaginationData>(props.api, { page: state.page, pageSize: state.pageSize, ...props.params })
+        if (data) {
+            state.data = data
+        }
         state.getDataLoading = false;
     } catch (error) {
         state.getDataLoading = false;
     }
 }
 
-onMounted(() => {
+const handlePageChange = (page: number) => {
+    state.page = page;
+    nextTick(() => {
+        getData()
+    })
+}
 
+const handlePageSizeChange = (pageSize: number) => {
+    state.pageSize = pageSize;
+    nextTick(() => {
+        getData()
+    })
+}
+
+const resetData = () => {
+    state.page = 1;
+    nextTick(() => {
+        getData()
+    })
+}
+
+onMounted(() => {
+    getData()
 })
 
 </script>
--- src/pages/profile/components/MyArticle.vue
+++ src/pages/profile/components/MyArticle.vue
@@ -6,16 +6,38 @@
 
 <script setup lang="ts">
 import RemoteTable from '@/components/RemoteTable.vue';
-import { apis } from '@/utils'
+import { apis, formatTime } from '@/utils'
+import { DataTableColumns, NSpace, NTag } from 'naive-ui';
+import { h } from 'vue';
+import { RouterLink } from 'vue-router';
 
-const tableColumns = [
+const tableColumns: DataTableColumns = [
     {
         title: '标题',
-        key: 'title'
+        key: 'title',
+        render(row: any) {
+            return h(RouterLink, {
+                to: `/blog/article?id=${row.id}`,
+                style: {
+                    color: "var(--primary-color)"
+                }
+            }, row.title)
+        }
     },
     {
         title: '简介',
         key: 'description'
+    },
+    {
+        title: "标签",
+        key: 'tags',
+        render(row: any) {
+            return h(NSpace, {
+                align: "center"
+            }, row.tags.map((item: any) => h(NTag, {
+                type: 'primary'
+            }, item.name)))
+        }
     },
     {
         title: '作者',
@@ -23,10 +45,14 @@ const tableColumns = [
     },
     {
         title: "上传时间",
-        key: 'createTime'
+        key: 'createTime',
+        render(row: any) {
+            return formatTime(row.createTime)
+        }
     },
     {
-        title: "操作"
+        title: "操作",
+        key: "control"
     }
 ]
 
