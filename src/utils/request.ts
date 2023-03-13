1f1727a000ab3b564f144fdf967ac41c2b9c982c
luopc1218 <luopc1218@outlook.com>
2023-03-13 18:23:57 +0800

    feat.new nginx confuration.

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,38 +1,42 @@
-import { AxiosRequestConfig, AxiosResponse } from "axios"
-import type { Api } from "./apis"
-import axios from "./axios"
-import { ResponseData, responseHandler } from "./responseHandler"
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import type { Api } from "./apis";
+import axios from "./axios";
+import { ResponseData, responseHandler } from "./responseHandler";
 
-
-
-export interface requestOptions extends Omit<AxiosRequestConfig, 'url' | 'method' | 'params' | 'data'> {
-    showErrorMessage?: boolean,
-    showSuccessMessage?: boolean
+export interface requestOptions
+  extends Omit<AxiosRequestConfig, "url" | "method" | "params" | "data"> {
+  showErrorMessage?: boolean;
+  showSuccessMessage?: boolean;
 }
 
 export interface PaginationData<T = any> {
-    list: T[],
-    totalCount: number
+  list: T[];
+  totalCount: number;
 }
 
-export const request = async <T = any,>(api: Api, params?: any, options?: requestOptions) => {
-    if (!api) {
-        console.error("api is not define!");
-        return Promise.reject(new Error("api is not define!"))
-    }
-    try {
-        const response: AxiosResponse<ResponseData<T>> = await axios({
-            url: api.url,
-            method: api.method,
-            params: api.method === 'get' ? params : undefined,
-            data: api.method === 'post' ? params : undefined,
-            ...options
-        })
+export const request = async <T = any>(
+  api: Api,
+  params?: any,
+  options?: requestOptions
+) => {
+  if (!api) {
+    console.error("api is not define!");
+    return Promise.reject(new Error("api is not define!"));
+  }
+  try {
+    const response: AxiosResponse<ResponseData<T>> = await axios({
+      url: api.url,
+      method: api.method,
+      timeout: 10000,
+      params: api.method === "get" ? params : undefined,
+      data: api.method === "post" ? params : undefined,
+      ...options,
+    });
 
-        return responseHandler(response.data, options)
-    } catch (error) {
-        return Promise.reject(error)
-    }
-}
+    return responseHandler(response.data, options);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+};
 
-export default request
+export default request;
--- vue.config.js
+++ vue.config.js
@@ -6,13 +6,13 @@ module.exports = defineConfig({
     proxy: {
       "/api": {
         // local
-        target: "http://localhost:8080", 
+        target: "http://localhost:8080",
         // dev
         // target: "http://myserver.com:8080",
         // prod
         // target: "http://localhost:8080",
         ws: true, //代理websockets
-        // changeOrigin: true, // 是否跨域，虚拟的站点需要修改origin
+        changeOrigin: true, // 是否跨域，虚拟的站点需要修改origin
         pathRewrite: {
           // '^/api'是一个正则表达式，表示要匹配请求的url中，全部'http://localhost:8081/api' 转接为 http://localhost:8081/
           "^/api": "",
@@ -22,8 +22,8 @@ module.exports = defineConfig({
   },
 
   runtimeCompiler: true,
-  assetsDir: 'public',
+  assetsDir: "public",
   css: {
-    sourceMap: true
-  }
+    sourceMap: true,
+  },
 });
